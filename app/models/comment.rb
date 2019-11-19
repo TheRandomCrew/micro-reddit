a6169d496fe0b55504d049ce9eee77e024e090bc
Israel-Laguan <israellaguan@gmail.com>
2019-11-19 12:15:17 -0500

    [fix] rubocop: fix errors
    What?:
    - Also add a destroy dependency for Post
    
    Breaking Changes?

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validates :comment, length: {  minimum: 2 }
+  validates :comment, length: { minimum: 2 }
   validates :user_id, :post_id, :comment, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :title, length: { in: 6..50 }
   validates :user_id, :body, :title, :image, presence: true
