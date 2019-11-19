fd384f690bc294ace4edd01d3ff29b4334955ff1
Israel-Laguan <israellaguan@gmail.com>
2019-11-19 11:42:37 -0500

    [feat] Models: Add Comment
    What?:
    - Add relations and test model
    
    Breaking Changes?

---
+++ app/models/comment.rb
@@ -0,0 +1,7 @@
+class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :post
+
+  validates :comment, length: {  minimum: 2 }
+  validates :user_id, :post_id, :comment, presence: true
+end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
 
-  validates :title, length: { in: 6..50 }, uniqueness: true
-  validates :body, :title, :image, presence: true
+  validates :title, length: { in: 6..50 }
+  validates :user_id, :body, :title, :image, presence: true
 end
